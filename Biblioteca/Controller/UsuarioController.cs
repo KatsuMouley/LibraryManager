cddc7db760a48dded7c850c6614bc0681d7c7869
matheus99tur <matheusborba12@hotmail.com>
2025-05-10 11:56:48 -0300

    Add, GerarToken

--- Biblioteca/Controller/UsuarioController.cs
+++ Biblioteca/Controller/UsuarioController.cs
@@ -12,54 +12,78 @@ namespace Biblioteca.Controller
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-    private readonly IUsuarioRepository _usuarioRepository;
-    private readonly IConfiguration _configuration;
-    public UsuarioController(IUsuarioRepository usuarioRepository, IConfiguration configuration)
-    {
-        _usuarioRepository = usuarioRepository;
-        _configuration = configuration;
-    }
-
-    [HttpPost("cadastrar")]
-    public IActionResult Cadastrar([FromBody] Usuario usuario)
-    {
-        _usuarioRepository.Cadastrar(usuario);
-        return Created("", usuario);
-    }
-
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] Usuario usuario)
-    {
-        Usuario? usuarioExistente = _usuarioRepository
-            .BuscarUsuarioPorEmailSenha(usuario.Email, usuario.Senha);
-        if(usuarioExistente == null)
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IConfiguration _configuration;
+        public UsuarioController(IUsuarioRepository usuarioRepository, IConfiguration configuration)
         {
-            return Unauthorized(new { mensagem = "Usuário ou senha inválidos!"});
+            _usuarioRepository = usuarioRepository;
+            _configuration = configuration;
         }
-            
-        // parte da implementação do Jwt
-        // string token = GerarToken(usuarioExistente);
-        return Ok(usuarioExistente);
-    }
 
-    [HttpGet("listar")]
-    public IActionResult Listar()
-    {        
-        return Ok(_usuarioRepository.Listar());
-    }
+        [HttpPost("cadastrar")]
+        public IActionResult Cadastrar([FromBody] Usuario usuario)
+        {
+            _usuarioRepository.Cadastrar(usuario);
+            return Created("", usuario);
+        }
 
-    // public string GerarToken(Usuario usuario)
-    // {
-    //     var claims = new[]
-    //     {
-    //         new Claim(ClaimTypes.Name, usuario.Email),
-    //         new Claim(ClaimTypes.Role, usuario.Permissao.ToString())
-    //     };
-    //     //Ainda não há chave Jwt
-    //     var chave = Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"]);
-    //     var assinatura = new SigningCredentials(new SymmetricSecurityKey(chave), SecurityAlgorithms.HmacSha256);
-    //     var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddSeconds(30), signingCredentials: assinatura);
-    //     return new JwtSecurityTokenHandler().WriteToken(token);
-    // }
+        // [HttpPost("login")]
+        // public IActionResult Login([FromBody] Usuario usuario)
+        // {
+        //     Usuario? usuarioExistente = _usuarioRepository
+        //         .BuscarUsuarioPorEmailSenha(usuario.Email, usuario.Senha);
+        //     if(usuarioExistente == null)
+        //     {
+        //         return Unauthorized(new { mensagem = "Usuário ou senha inválidos!"});
+        //     }
+
+        //     // parte da implementação do Jwt
+        //     // string token = GerarToken(usuarioExistente);
+        //     return Ok(usuarioExistente);
+        // }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Usuario usuario)
+        {
+            Usuario? usuarioExistente = _usuarioRepository
+                .BuscarUsuarioPorEmailSenha(usuario.Email, usuario.Senha);
+
+            if (usuarioExistente == null)
+            {
+                return Unauthorized(new { mensagem = "Usuário ou senha inválidos!" });
+            }
+
+            string token = GerarToken(usuarioExistente);
+
+            return Ok(new
+            {
+                token = token,
+                usuario = new
+                {
+                    id = usuarioExistente.Id,
+                    email = usuarioExistente.Email
+                }
+            });
+        }
+
+
+        [HttpGet("listar")]
+        public IActionResult Listar()
+        {
+            return Ok(_usuarioRepository.Listar());
+        }
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public string GerarToken(Usuario usuario)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, usuario.Email),
+            new Claim(ClaimTypes.Role, usuario.Permissao.ToString())
+        };
+            //Ainda não há chave Jwt
+            var chave = Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"]!);
+            var assinatura = new SigningCredentials(new SymmetricSecurityKey(chave), SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddSeconds(30), signingCredentials: assinatura);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a8f01e2f3e87afa69c3715b2211ebf03dcad782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e991211f8ae96fefcfc02de08f1c18bde94a8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
